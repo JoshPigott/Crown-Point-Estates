3ad09737c87ed40770ab600aff95741b1e4a4faf
Josh Pigott <joshuapigott10@icloud.com>
2026-03-16 08:27:39 +1300

    css added to listing page. media queries added

--- src/views/listing.js
+++ src/views/listing.js
@@ -1,21 +1,5 @@
 import { escapeHtml } from "../utils/escape-html.js";
 
-// Returns a detail version of the listing
-export function listingFullView(listing) {
-  const html = /*html*/ `
-    <h1>${escapeHtml(listing?.name)}</h1>
-    <h2>${escapeHtml(listing?.address)}</h2>
-    <h2>${escapeHtml(listing?.area)}</h2>
-    <h3>${escapeHtml(listing?.description)}</h3>
-    <img src="/assets/listings-pics/${
-    escapeHtml(listing?.imageFileName)
-  }.jpg" alt="${escapeHtml(listing?.name)} image">
-    <h3>Price: $${listing?.price}</h3>
-    <h3>Rating: ${listing?.rating}/10</h3>
-  `;
-
-  return html;
-}
 // A more conise listing
 export function listingView(listing) {
   const html = /*html*/ `
--- src/views/listings/listings-page.js
+++ src/views/listings/listings-page.js
@@ -1,5 +1,69 @@
-import { listingFullView } from "../listing.js";
+import { escapeHtml } from "../../utils/escape-html.js";
 
+// Adds listing to watchlist
+export function addButtonView(listingId) {
+  return /*html*/ `<button hx-post="/watch-list-add/${listingId}" hx-swap="outerHTML" class="watchlist__buttons">Add To Watch List</button>`;
+}
+
+// Removes listing from watchlist
+export function removeButtonView(listingId) {
+  return /*html*/ `<button hx-delete="/watch-list-delete/${listingId}" hx-swap="outerHTML" class="watchlist__buttons">Remove From Watch List</button>`;
+}
+
+// Returns a detail version of the listing
+export function listingFullView(listing, inWatchlist) {
+  const html = /*html*/ `
+    <img class="listing-full__image" 
+    src="/assets/listings-pics/${escapeHtml(listing?.imageFileName)}.jpg"
+    alt="${escapeHtml(listing?.name)} image">
+    
+    <div class="listing-full__text">
+      <div class="listing-full__title">
+        <h2>${escapeHtml(listing?.name)}</h2>
+        <!-- Only puts common in if there is an address-->
+        <h3>${escapeHtml(listing?.address)} ${
+    listing?.address !== "" ? "," : ""
+  } ${escapeHtml(listing?.area)}</h3>
+        <p class="listing-full__description">${
+    escapeHtml(listing?.description)
+  }</p>
+    
+      </div>
+  
+      
+      
+      <div class="listing-full__details">
+        <h3>Details</h3>
+        <div class="listing-full__details-info">
+        <p>List Price: $${listing?.price?.toLocaleString()} NZD</p>
+        <p>Status: ${listing?.status}</p>
+        <p>Rating: ${listing?.rating}/10</p>
+        <p>Veiwings: By Appointment</p>
+        </div>
+      </div>
+  
+      <div class="listing-full__contact">
+        <h3>Contact</h3>
+        <div class="listing-full__contact-info">
+        <p>Crown Point Estates Team</p>
+        <p><a class="footer__link" href="mailto:crownpointestates@gmail.com"
+        >crownpointestates@gmail.com</a></p>
+        <p>+64 21 784 7129</p>
+        </div>
+      </div>
+
+      <div class="listing-full__button">
+        ${
+    inWatchlist ? removeButtonView(listing?.id) : addButtonView(listing?.id)
+  }
+      </div>
+    <div>
+  `;
+
+  return html;
+}
+
+// The listing page itself
 function listingPageView(listing, inWatchlist) {
   return /*html*/ `
   <!DOCTYPE html>
@@ -19,11 +83,7 @@ function listingPageView(listing, inWatchlist) {
         <a href="/watchlist.html" class="header__link">Watch list</a>
       </header>
       <main>
-        <div>This is an listing</div>
-        ${listingFullView(listing)}
-        ${
-    inWatchlist ? removeButtonView(listing?.id) : addButtonView(listing?.id)
-  }
+        ${listingFullView(listing, inWatchlist)}
       </main>
       <footer>
       <div class="footer__description">
@@ -44,15 +104,6 @@ function listingPageView(listing, inWatchlist) {
   </html>
 
     `;
-}
-// Adds listing to watchlist
-export function addButtonView(listingId) {
-  return /*html*/ `<button hx-post="/watch-list-add/${listingId}" hx-swap="outerHTML">Add To Watch List</button>`;
-}
-
-// Removes listing from watchlist
-export function removeButtonView(listingId) {
-  return /*html*/ `<button hx-delete="/watch-list-delete/${listingId}" hx-swap="outerHTML">Remove From Watch List</button>`;
 }
 
 export default listingPageView;
